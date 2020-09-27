bcbcd847b4d451f1a6a9c4e859bb17eb139f5d24
JunSeung Mo <mojuns@gmail.com>
2020-09-27 15:45:42 +0900

    로그인 클릭시 ID / PW 받아내기

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -2,14 +2,24 @@ package kr.co.tjoeun.daily10minutes_20200927
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+            val inputID = idEdt.text.toString()
+            val inputpw = pwEdt.text.toString()
+
+        }
 
     }
 
