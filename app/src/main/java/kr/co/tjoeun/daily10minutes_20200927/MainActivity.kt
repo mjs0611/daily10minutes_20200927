7184722367b8857bda94e11962673ce614769fa0
JunSeung Mo <mojuns@gmail.com>
2020-10-10 14:16:20 +0900

    서버 데이터 저장용 프로젝트 데이터클래스 추가

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -1,21 +1,22 @@
 package kr.co.tjoeun.daily10minutes_20200927
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
-
-        override fun setupEvents() {
-
-        }
-
-        override fun setValues() {
-
-        }
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
