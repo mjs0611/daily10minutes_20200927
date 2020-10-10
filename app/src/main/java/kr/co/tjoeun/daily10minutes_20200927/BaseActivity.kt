aaef84bd7c0fcf28f3f2b3e97f21323385bbf9ec
JunSeung Mo <mojuns@gmail.com>
2020-10-10 11:27:10 +0900

    로그인 실패 시 토스트 출력

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/BaseActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/BaseActivity.kt
@@ -2,7 +2,7 @@ package kr.co.tjoeun.daily10minutes_20200927
 
 import androidx.appcompat.app.AppCompatActivity
 
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
 
     val mContext = this
 
