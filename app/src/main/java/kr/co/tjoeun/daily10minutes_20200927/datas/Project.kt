0434c9cef98423458353f865ac8a34677ad0c409
JunSeung Mo <mojuns@gmail.com>
2020-10-10 15:38:06 +0900

    리스트뷰 / 어댑터 연결 + 서버에서 다녀온 경우 리스트뷰 새로고침

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/datas/Project.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/datas/Project.kt
@@ -4,8 +4,8 @@ class Project {
 
 //    서버에서 내려주는 참여가능 프로젝트의 데이터(하위정보) 를 담기 위한 변수들
 
-    val id = 0 // id는 Int라고 명시
-    val title = "" // title은 String이라고 명시
-    val imageURL = ""
+    var id = 0 // id는 Int라고 명시
+    var title = "" // title은 String이라고 명시
+    var imageURL = ""
     var desc = ""
 }
