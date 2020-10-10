4fb42e27715b1e058128fd499b2952e61ef5b1ed
JunSeung Mo <mojuns@gmail.com>
2020-10-10 15:48:03 +0900

    리스트뷰 데이터 출력 마무리

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/MainActivity.kt
@@ -10,9 +10,8 @@ import org.json.JSONObject
 class MainActivity : BaseActivity() {
 
     val mProjectList = ArrayList<Project>()
-    
-    lateinit var mAdapter: ProjectAdapter
 
+    lateinit var mAdapter : ProjectAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +25,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-         getProjectListFromServer()
+        getProjectListFromServer()
 
         mAdapter = ProjectAdapter(mContext, R.layout.project_list_item, mProjectList)
         projectListView.adapter = mAdapter
 
     }
 
-//    서버에서 프로젝트 목록 가져오는 코드를 별도 함수로 분리
+//    서버에서 프로젝트 목록 가져오는 코드를 별도 함수로 분리.
 
     fun getProjectListFromServer() {
 
@@ -43,36 +42,40 @@ class MainActivity : BaseActivity() {
             override fun onResponse(json: JSONObject) {
 
                 val dataObj = json.getJSONObject("data")
-                val projectArr = dataObj.getJSONArray("projects")
+                val projectsArr = dataObj.getJSONArray("projects")
 
-//                projectArr의 내용물을 0~끝번까지 뽑아내자
+//                projectsArr의 내용물을 0~끝번까지 뽑아내자.
 
-                for (i in      0 until projectArr.length())
+                for (i in    0 until projectsArr.length()) {
 
 //                    상황에 맞는 JSONObject를 추출
-                    val projectObj = projectArr.getJSONObject(i)
+                    val projectObj = projectsArr.getJSONObject(i)
 
-//                    추출된 JSONObject를 가지고 => Project 클래스로 변환
+//                    추출된 JSONObject를 가지고 => Project 클래스로 변환.
 
                     val tempProject = Project()
                     tempProject.id = projectObj.getInt("id")
-                    tempProject.title = projectObj.getString("totle")
-                    tempProject.imageURL = projectObj.getString("Img_url")
+                    tempProject.title = projectObj.getString("title")
+                    tempProject.imageURL = projectObj.getString("img_url")
                     tempProject.desc = projectObj.getString("description")
 
-//                    완성된 Project 클래스를 => mProjectList에 추가
+//                    완성된 Project 클래스를 => mProjectList 에 추가.
 
                     mProjectList.add(tempProject)
-//              서버에 다녀오는 작업이, 어댑터 연결보다 늦게 완료될 수 있다. (거리상 문제)
-//              서버에 다녀오면 어댑터 연결 이후 => 데이터 추가 상황 발생 가능
-//              내용물로 쓰이는 목록에 변경 발생 => 리스트뷰 새로고침 코드 추가
 
-                    runOnUiThread{
-                        mAdapter.notifyDataSetChanged()
-                    }
+                }
+
+//                서버에 다녀오는 작업이, 어댑터 연결보다 늦게 완료될 수 있다. (거리상 문제)
+//                서버에 다녀오면 어댑터 연결 이후 => 데이터 추가 상황 발생 가능.
+//                내용물로 쓰이는 목록에 변경 발생. => 리스트뷰 새로고침 코드 추가
+
+                runOnUiThread {
+                    mAdapter.notifyDataSetChanged()
+                }
+
+
+
             }
-
-
 
         })
 
--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProjectAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProjectAdapter.kt
@@ -1,33 +1,48 @@
 package kr.co.tjoeun.daily10minutes_20200927.adapters
 
 import android.content.Context
+import android.gesture.GestureLibrary
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import kr.co.tjoeun.daily10minutes_20200927.R
 import kr.co.tjoeun.daily10minutes_20200927.datas.Project
 
 class ProjectAdapter(
     val mContext: Context,
-    resID: Int,
-    val mList: List<Project>) : ArrayAdapter<Project>(mContext, resID, mList) {
-    
+    resId: Int,
+    val mList: List<Project>) : ArrayAdapter<Project>(mContext, resId, mList) {
+
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        
+
         var tempRow = convertView
-        
+
         if (tempRow == null) {
             tempRow = inf.inflate(R.layout.project_list_item, null)
         }
-        
+
         val row = tempRow!!
-        
-//        실제 데이터 반영 필요
-        
+
+//        뿌려줄 데이터
+        val projectData = mList[position]
+
+//        내용이 뿌려질 뷰 가져오기
+        val projectImg = row.findViewById<ImageView>(R.id.projectImg)
+        val projectTitleTxt = row.findViewById<TextView>(R.id.projectTitleTxt)
+        val projectDescTxt = row.findViewById<TextView>(R.id.projectDescTxt)
+
+//        실 데이터 반영
+        projectTitleTxt.text = projectData.title
+        projectDescTxt.text = projectData.desc
+
+        Glide.with(mContext).load(projectData.imageURL).into(projectImg)
+
         return row
-        
     }
+
 }
