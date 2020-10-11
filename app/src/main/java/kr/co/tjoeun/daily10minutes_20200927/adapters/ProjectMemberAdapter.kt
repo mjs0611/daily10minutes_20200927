5d68c551d837882f2708e624456d44e224ff0b1f
JunSeung Mo <mojuns@gmail.com>
2020-10-11 15:01:53 +0900

    실제 이메일 / 닉네임 출력

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProjectMemberAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProjectMemberAdapter.kt
@@ -7,15 +7,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import com.bumptech.glide.Glide
 import kr.co.tjoeun.daily10minutes_20200927.R
-import kr.co.tjoeun.daily10minutes_20200927.datas.Project
 import kr.co.tjoeun.daily10minutes_20200927.datas.User
 
 class ProjectMemberAdapter(
     val mContext: Context,
     resId: Int,
-    val mList: List<User) : ArrayAdapter<User>(mContext, resId, mList) {
+    val mList: List<User>) : ArrayAdapter<User>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
@@ -28,6 +26,15 @@ class ProjectMemberAdapter(
         }
 
         val row = tempRow!!
+
+        val userData = mList[position]
+
+        val userProfileImg = row.findViewById<ImageView>(R.id.userProfileImg)
+        val userNickNameTxt = row.findViewById<TextView>(R.id.userNickNameTxt)
+        val userEmailTxt = row.findViewById<TextView>(R.id.userEmailTxt)
+
+        userNickNameTxt.text = userData.nickName
+        userEmailTxt.text = userData.email
 
         return row
     }
