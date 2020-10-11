fd7ea23038013d12d0ac91c5dc5022f581848831
JunSeung Mo <mojuns@gmail.com>
2020-10-11 11:47:47 +0900

    프로젝트 상세보기 화면 작업 중   - 이미지, 제목, 설명만 우선 표기

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/ViewProjectDetailActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.daily10minutes_20200927
+
+import android.os.Bundle
+
+class ViewProjectDetailActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_project_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
