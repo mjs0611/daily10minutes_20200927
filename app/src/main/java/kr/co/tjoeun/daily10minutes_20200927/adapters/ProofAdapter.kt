5d1b7b32a00a5b284047dc8a950ee9dbc76bdbb4
JunSeung Mo <mojuns@gmail.com>
2020-10-17 14:53:54 +0900

    drawable xml을 이용해서 빨간 테두리 박스 구현  - 손으로 누를때 / 아닐때(기본상태) 구별해서 색 변경

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProofAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ProofAdapter.kt
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.daily10minutes_20200927.adapters
 
 import android.content.Context
+import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,6 @@ import kr.co.tjoeun.daily10minutes_20200927.datas.Proof
 import kr.co.tjoeun.daily10minutes_20200927.utils.ServerUtil
 import org.json.JSONObject
 import java.text.SimpleDateFormat
-import java.util.logging.Handler
 
 class ProofAdapter(
     val mContext: Context,
@@ -33,8 +33,8 @@ class ProofAdapter(
 
         val proofData = mList[position]
 
-        val writeProfileImg = row.findViewById<ImageView>(R.id.writerProfileImg)
-        val writeNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
+        val writerProfileImg = row.findViewById<ImageView>(R.id.writerProfileImg)
+        val writerNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
         val writtenDateTimeTxt = row.findViewById<TextView>(R.id.writtenDateTimeTxt)
         val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
         val proofImg = row.findViewById<ImageView>(R.id.proofImg)
@@ -43,23 +43,22 @@ class ProofAdapter(
 
         contentTxt.text = proofData.content
 
-        Glide.with(mContext).load(proofData.writer.profileImageList[0]).into(writeProfileImg)
-        writeNickNameTxt.text = proofData.writer.nickName
+        Glide.with(mContext).load(proofData.writer.profileImageList[0]).into(writerProfileImg)
+        writerNickNameTxt.text = proofData.writer.nickName
 
 //        인증글 시간정보 => 2020년 5월 8일 오후 3시 1분 양식으로 출력
         val sdf = SimpleDateFormat("yyyy년 M월 d일 a h시 m분")
         writtenDateTimeTxt.text = sdf.format(proofData.proofTime.time)
 
 //        인증글의 이미지가 0개 : 이미지뷰 숨김
-//        그렇지 않다 (1개 이상) : 이미지뷰 보여주기 + Glide 이미지 세팅
+//        그렇지 않다 (1개 이상) : 이미지뷰 보여주기 + Glide 이미지 세팅 (편의상 0번째)
 
         if (proofData.imageList.size == 0) {
             proofImg.visibility = View.GONE
         }
         else {
             proofImg.visibility = View.VISIBLE
-            Glide.with(mContext).load(proofData.imageList[0]).into(proofImg
-            )
+            Glide.with(mContext).load(proofData.imageList[0]).into(proofImg)
         }
 
 //        좋아요 갯수
@@ -69,22 +68,36 @@ class ProofAdapter(
 //        좋아요 클릭 이벤트 처리
         likeBtn.setOnClickListener {
 
-            ServerUtil.postRequestLikeProof(mContext, proofData.id, object : ServerUtil.JsonResponseHandler{
+            ServerUtil.postRequestLikeProof(mContext, proofData.id, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
+
+//                    서버에서 주는 응답을 보면 이 인증글의 최신상태를 내려줌.
+//                    이 글을 파싱해서 => 리스트 내용 일부 변경
+
+//                    proofData 변수가 목록에 등장 => proofData 변수의 일부 내용 변경 : 목록에 변경 반영
+
+                    proofData.likeCount = json.getJSONObject("data").getJSONObject("like").getInt("like_count")
+                    proofData.isMyLike = json.getJSONObject("data").getJSONObject("like").getBoolean("my_like")
+
                     val message = json.getString("message")
 
-//                    어댑터는 액티비티가 아님. runOnUiThread 기능을 가족있지 않다
-//                    그럼에도 UI쓰레드에서 토스트를 뜨ㅢ어야함
-//                    쓰레드처럼 동작 : Handler를 이용햐 UIThread 에 접근
+//                    어댑터는 액티비티가 아님. runOnUiThread 기능을 갖고있지 않다.
+//                    그럼에도 UI쓰레드에서 토스트를 띄워야함.
+//                    쓰레드처럼 동작 : Handler 를 이용해 UIThread에 접근하자.
 
-//                    getMiainLooper를 통해 이 Handler는 UI쓰레드로 접근
-                    val myHandler = Handler(Looper.gerMainLooper())
+//                    getMainLooper 를 통해 이 Handler는 UI쓰레드로 접근
+                    val myHandler = Handler(Looper.getMainLooper())
 
+//                    UI쓰레드에 post {  } 내부의 코드를 실행하도록 요청
                     myHandler.post {
-                        Toast.makeText((mContext, message, Toast.LENGTH_SHORT)).show()
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+//                        변수의 변경 내용을 리스트뷰에 반영
+                        notifyDataSetChanged()
                     }
 
                 }
+
             })
 
         }
