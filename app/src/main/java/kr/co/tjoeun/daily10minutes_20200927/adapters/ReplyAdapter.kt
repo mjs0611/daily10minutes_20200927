2a01220058f85482d2bcf9c93a23c1d72924f837
JunSeung Mo <mojuns@gmail.com>
2020-10-18 12:05:40 +0900

    댓글 리스트 아이템 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200927/adapters/ReplyAdapter.kt
@@ -0,0 +1,31 @@
+package kr.co.tjoeun.daily10minutes_20200927.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.tjoeun.daily10minutes_20200927.R
+import kr.co.tjoeun.daily10minutes_20200927.datas.Reply
+
+class ReplyAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.project_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
